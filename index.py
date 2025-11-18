4e3efc10e1769d359f0edaafdbca6340c462d8e3
Zhukovskyi Danyil <vtk251_zhdo@student.ztu.edu.ua>
2025-11-18 10:42:58 +0200

    Друге завдання

--- index.py
+++ index.py
@@ -27,3 +27,40 @@ def Task1():
 
     print("Максимальний елемент списку: ", max_value)
     print("Список у зворотному порядку: ", numbers[::-1])
+
+def Task2():
+
+    n = input("Введіть кількість елементів списку: ")
+
+    if not n.isdigit():
+        print("Помилка: потрібно ввести додатне число!")
+        exit()
+
+    n = int(n)
+
+    if n <= 0:
+        print("Помилка: кількість елементів повинна бути більшою за 0!")
+        exit()
+
+    numbers = []
+    for i in range(n):
+        value = input(f"Введіть елемент №{i + 1}: ")
+
+        if value.lstrip("-").isdigit():
+            numbers.append(int(value))
+        else:
+            print("Помилка: введено не ціле число!")
+            exit()
+
+    positive = []
+    others = []
+
+    for num in numbers:
+        if num > 0:
+            positive.append(num)
+        else:
+            others.append(num)
+
+    print("Початковий список: ", numbers)
+    print("Додатні елементи: ", positive)
+    print("Інші елементи (нуль і від’ємні): ", others)
