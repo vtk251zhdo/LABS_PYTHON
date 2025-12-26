b76eabe0abdc32be3a0b2f233bdc0f17e9799465
Zhukovskyi Danyil <vtk251_zhdo@student.ztu.edu.ua>
2025-12-26 09:04:51 +0200

    Function route pages

--- app.py
+++ app.py
@@ -10,3 +10,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///frostwave.db'
 
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    if not User.query.first():
+        admin = User(username="admin", password="1234")
+        db.session.add(admin)
+        db.session.commit()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+@app.route("/albums")
+def albums():
+    return render_template("albums.html", albums=Album.query.all())
